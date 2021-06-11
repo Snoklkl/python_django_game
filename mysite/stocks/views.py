f41d4a4d43649b5e664d17a0bde680c48e8f7598
Noah <Snoklkl@users.noreply.github.com>
2021-06-11 00:10:44 -0500

    views and stocks index.html

--- mysite/stocks/views.py
+++ mysite/stocks/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import stocks_info, player_info, player_targets, time_trackers
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'stocks/index.html')
