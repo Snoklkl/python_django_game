2e32359afcc8375d70299c3a4f4fa4f180c6100b
Noah <Snoklkl@users.noreply.github.com>
2021-06-12 01:58:24 -0500

    began structuring how will handle receiving and giving data

---
+++ mysite/stocks/static/stocks/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $("#queryPlayer").on("click", function() {
+
+        var playerYear = $("#yearInput").val().trim();
+        
+        var playerName = $("#nameInput").val().trim();
+
+
+    })
+
+
+
+
+
+
+})
